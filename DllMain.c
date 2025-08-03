1d185cce094ada07021746bfba90db39d789b472
Thamatip Chitpong <tangaming123456@outlook.com>
2025-08-03 23:46:29 +0700

    Simplify DllMain

--- DllMain.c
+++ DllMain.c
@@ -17,26 +17,6 @@
 
 BOOL WINAPI DllMain(HINSTANCE hInstDLL, DWORD fdwReason, LPVOID lpReserved)
 {
-    /* Perform actions based on the reason for calling. */
-    switch (fdwReason)
-    {
-        /* Initialize once for each new process, Return FALSE to fail DLL load. */
-        case DLL_PROCESS_ATTACH:
-            break;
-
-        /* Do thread-specific initialization. */
-        case DLL_THREAD_ATTACH:
-            break;
-
-        /* Do thread-specific cleanup. */
-        case DLL_THREAD_DETACH:
-            break;
-
-        /* Perform any necessary cleanup. */
-        case DLL_PROCESS_DETACH:
-            break;
-    }
-
-    return TRUE;  // Successful DLL_PROCESS_ATTACH.
+    /* This is just a dummy DLL */
+    return TRUE;
 }
-
