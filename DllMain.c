6f56b403c73e6915e55bb5da1944027e0f04aa3f
Thamatip Chitpong <tangaming123456@outlook.com>
2025-08-03 23:54:36 +0700

    Change license to 0BSD

--- DllMain.c
+++ DllMain.c
@@ -1,16 +1,8 @@
 /*
- * Win7-GamesExplorerBlocker - Disable Windows 7 Games Explorer completely and
- * remove rundll32.exe process from your games.
+ * Author: Thamatip Chitpong, 2021-2025
  *
- * Written in 2021 by Thamatip Chitpong <tangaming123456@outlook.com>
- *
- * To the extent possible under law, the author(s) have dedicated all copyright
- * and related and neighboring rights to this software to the public domain
- * worldwide. This software is distributed without any warranty.
- *
- * You should have received a copy of the CC0 Public Domain Dedication along
- * with this software. If not, see
- * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ * SPDX-License-Identifier: 0BSD
+ * License-Filename: LICENSE.txt
  */
 
 #include <windows.h>
